67098813b97090a248cf1b875b00d3b48817cd3f
fr6462003 <finlayryder@gmail.com>
2024-12-11 16:45:24 +0000

    Add files via upload

--- question-4-code/random_walk.R
+++ question-4-code/random_walk.R
@@ -1,34 +1,39 @@
-#install.packages("ggplot2")
-#install.packages("gridExtra")
-
 library(ggplot2)
 library(gridExtra)
 
-random_walk  <- function (n_steps) {
+brownian_motion  <- function (n_steps, seed = 12345, rnorm_mean = 0,
+                              step_sd = 1, drift = 0) {
   
-  df <- data.frame(x = rep(NA, n_steps), y = rep(NA, n_steps), time = 1:n_steps)
+  df <- data.frame(x = numeric(n_steps), y = numeric(n_steps),  
+                    time = seq_len(n_steps)) 
+  
+  set.seed(seed)
   
   df[1,] <- c(0,0,1)
   
-  for (i in 2:n_steps) {
-    
-    h <- 0.25
-    
-    angle <- runif(1, min = 0, max = 2*pi)
-    
-    df[i,1] <- df[i-1,1] + cos(angle)*h
-    
-    df[i,2] <- df[i-1,2] + sin(angle)*h
-    
-    df[i,3] <- i
-    
-  }
+  h_x <- rnorm(n_steps - 1, mean = rnorm_mean + drift, sd = step_sd)
+  h_y <- rnorm(n_steps - 1, mean = rnorm_mean + drift, sd = step_sd)
+  
+  angle_change <- rnorm(n_steps - 1, mean = rnorm_mean, sd = step_sd)
+  cos_offsets <- cos(angle_change)
+  sin_offsets <- sin(angle_change)
+  
+  x_steps <- cos_offsets + h_x
+  y_steps <- sin_offsets + h_y
+  
+  x_positions <- c(0, cumsum(x_steps))
+  y_positions <- c(0, cumsum(y_steps))
+  
+  df$x <- x_positions
+  df$y <- y_positions
   
   return(df)
-  
-}
 
-data1 <- random_walk(500)
+}
+  
+
+
+data1 <- brownian_motion(500)
 
 plot1 <- ggplot(aes(x = x, y = y), data = data1) +
   
@@ -40,7 +45,7 @@ plot1 <- ggplot(aes(x = x, y = y), data = data1) +
   
   ylab("y-coordinate")
 
-data2 <- random_walk(500)
+data2 <- brownian_motion(500)
 
 plot2 <- ggplot(aes(x = x, y = y), data = data2) +
   
@@ -53,3 +58,4 @@ plot2 <- ggplot(aes(x = x, y = y), data = data2) +
   ylab("y-coordinate")
 
 grid.arrange(plot1, plot2, ncol=2)
+
